df43a6f15a34b004444563ad87a296aa84e34af5
Chris Stead <cmstead@gmail.com>
2020-11-16 14:35:44 -0800

    e Removed unused publish script

--- scripts/publish.js
+++
@@ -1,9 +0,0 @@
-const childProcess = require('child_process');
-const util = require('util');
-
-const exec = util.promisify(childProcess.exec);
-
-const versionType = process.argv.slice(2)[0];
-
-childProcess.execSync(`npm version ${versionType}`, { stdio: "inherit" });
-childProcess.execSync(`npm publish`, { stdio: "inherit" });
