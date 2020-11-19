094367f583867c6b0ce9061278665beca6191388
Chris Stead <cmstead@gmail.com>
2020-11-18 16:24:46 -0800

    F Adding merge to temp branch

--- modules/branch/merge-to-temp.js
+++ modules/branch/merge-to-temp.js
@@ -1,6 +1,8 @@
 const childProcess = require('child_process');
 const util = require('util');
 
+const inquirer = require('inquirer');
+
 const { readBranchNames, selectBranch, attemptMerge, getCurrentBranch } = require('./branch-utils');
 
 function checkoutBranch(branchName) {
@@ -9,8 +11,21 @@ function checkoutBranch(branchName) {
     return util.promisify(childProcess.exec)(gitCommand);
 }
 
+function getTempBranchName() {
+    return inquirer.prompt([
+        {
+            name: 'tempBranchName',
+            message: 'Name of the temp branch to merge to'
+        }
+    ])
+    .then(function(result){
+        return result.getTempBranchName;
+    });
+}
+
 function mergeToBranch() {
     let currentBranch = '';
+    let sourceBranch = '';
 
     return getCurrentBranch()
         .then(function (branchName) {
@@ -20,16 +35,19 @@ function mergeToBranch() {
             return readBranchNames();
         })
         .then(function (branchNames) {
-            return selectBranch(branchNames, "Select branch to merge to");
+            return selectBranch(branchNames, "Select branch to merge from");
+        })
+        .then(function (sourceBranchName) {
+            sourceBranch = sourceBranchName;
+        })
+        .then(function () {
+            return getTempBranchName();
         })
         .then(function (branchName) {
             return checkoutBranch(branchName)
         })
         .then(function () {
-            return attemptMerge(currentBranch);
-        })
-        .finally(function() {
-            checkoutBranch(currentBranch);
+            return attemptMerge(sourceBranch);
         });
 }
 
